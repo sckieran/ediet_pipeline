16295d308aae81550b1add356bc208b82ca33d41
sckieran <53580356+sckieran@users.noreply.github.com>
2024-08-14 12:13:13 -0700

    Update new_step_6_blast_remote.sh

--- scripts/new_step_6_blast_remote.sh
+++ scripts/new_step_6_blast_remote.sh
@@ -22,7 +22,12 @@ while getopts ":n:g:d:m:c:t:e:" opt; do
     esac
 done
 
-source activate ${env_name}
+
+##load conda
+conda_source=$(conda info | grep -i 'base environment' | awk -F":" '{print $2}'| awk -F" " '{print $1}')
+source ${conda_source}/etc/profile.d/conda.sh
+conda activate ${env_name}
+#source activate ${env_name}
 pref=$CONDA_PREFIX
 
 mkdir -p ${gene}_out
