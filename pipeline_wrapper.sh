9ec557df0f365af010ab09c130ffc285ee4bbdf3
sckieran <53580356+sckieran@users.noreply.github.com>
2024-08-15 13:50:37 -0700

    Update pipeline_wrapper.sh

--- pipeline_wrapper.sh
+++ pipeline_wrapper.sh
@@ -80,7 +80,7 @@ do
   echo "beginning pears F/R read mergers on 4 threads for gene $p"
   echo "###"
   
-  bash ${dir}/scripts/step_3_pears.sh ${R1_pattern} ${R2_pattern} ${dir} ${max_jobs} ${user} ${gene}
+  bash ${dir}/scripts/step_3_pears.sh ${R1_pattern} ${R2_pattern} ${dir} ${max_jobs} ${user} ${gene} ${env_name}
   exit_status=$?
   if [ "${exit_status}" -ne 0 ];
   then
